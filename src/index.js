e6c8300999d9ab393674931e5bff308780833276
neo-30003 <dr.arrum@gmail.com>
2024-08-21 00:54:38 +0300

    Таймер акции

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import modalRequest from "./modules/modalRequest";
 import { benefitsSlider, serviceSlider } from "./modules/sliders";
 import serviceModal from "./modules/serviceModal";
+import timer from "./modules/timer";
 
 modalRequest();
 benefitsSlider();
 serviceSlider();
 serviceModal();
+timer("10 september 2024");

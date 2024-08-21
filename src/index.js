56ab7e1dd3a35b5d723d39548776bd47fb38924f
neo-30003 <dr.arrum@gmail.com>
2024-08-21 17:43:07 +0300

    Плавная прокрутка вверх

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import timer from "./modules/timer";
 import sendForm from "./modules/sendForm";
 import validateForm from "./modules/validateForm";
 import certificates from "./modules/certificates";
+import scroll from "./modules/scroll";
 
 modalRequest();
 benefitsSlider();
@@ -14,3 +15,4 @@ timer("10 september 2024");
 sendForm();
 validateForm();
 certificates();
+scroll();
